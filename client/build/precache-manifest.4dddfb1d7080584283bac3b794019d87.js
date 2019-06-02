aa6df7a47ea8510cd93f04ddc680f3d94e33ba86
Rostislav <rostislav.futornoy@gmail.com>
2019-06-02 17:23:20 +0300

    .gitignore changed

---
+++ client/build/precache-manifest.4dddfb1d7080584283bac3b794019d87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f15e57afa7b9897266d76b047d32fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86d1d4fdc01516e05e66",
+    "url": "/static/css/main.0d4e3bb5.chunk.css"
+  },
+  {
+    "revision": "a60c12d9e8eb2d297e47",
+    "url": "/static/js/2.ea8bdd8e.chunk.js"
+  },
+  {
+    "revision": "86d1d4fdc01516e05e66",
+    "url": "/static/js/main.54773715.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
