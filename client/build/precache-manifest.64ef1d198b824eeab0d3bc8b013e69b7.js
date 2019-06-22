fe383a4a16e1da6702a96b4cc7679d22c2da2e23
Rostislav <rostislav.futornoy@gmail.com>
2019-06-22 16:54:02 +0300

    Added new build

--- client/build/precache-manifest.64ef1d198b824eeab0d3bc8b013e69b7.js
+++ client/build/precache-manifest.64ef1d198b824eeab0d3bc8b013e69b7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "04bd94af1a7c92eb423aca283dbfde60",
+    "revision": "ef07e6010dfa8e7fba9499ef00d22304",
     "url": "/index.html"
   },
   {
-    "revision": "69ed2af17eb6f8750fdf",
+    "revision": "e6eb8ee147372690341e",
     "url": "/static/css/main.0d4e3bb5.chunk.css"
   },
   {
-    "revision": "6d7eab10a4a8f936e8f3",
-    "url": "/static/js/2.dcb5b77b.chunk.js"
+    "revision": "43db24b385868285834b",
+    "url": "/static/js/2.c50b0105.chunk.js"
   },
   {
-    "revision": "69ed2af17eb6f8750fdf",
-    "url": "/static/js/main.8a4a975d.chunk.js"
+    "revision": "e6eb8ee147372690341e",
+    "url": "/static/js/main.b32f23bb.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.560f39bf73bdc069b13c1a2e729332a8.js"
+  "/precache-manifest.64ef1d198b824eeab0d3bc8b013e69b7.js"
 );
 
 self.addEventListener('message', (event) => {
